a6f12cd9b197be5910d6962732f34f9515518486
Miotis2004 <miotis2004@gmail.com>
2020-11-10 18:52:40 -0500

    First Commit

---
+++ Fruits/Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Field Employee on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
